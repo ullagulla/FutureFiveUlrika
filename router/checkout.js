7266352ea20f8cf274a24e30f8610e39effa82f8
ullagulla <ulrikaa91@hotmail.com>
2020-04-04 14:26:48 +0200

    changes to stripe secret key

--- router/checkout.js
+++ router/checkout.js
@@ -6,7 +6,7 @@ const verifyToken = require("./verify")
 const Product = require("../models/product")
 const Order = require("../models/order")
 
-const stripe = require('stripe')(config.Stripe_Secret_Key);
+const stripe = require('stripe')("pk_test_FPS2Na5LzZxYFLdI7aAuwWt100ehbVLP3F");
 
 router.get("/checkout", verifyToken, async (req, res) => {
 
@@ -35,7 +35,7 @@ router.get("/checkout", verifyToken, async (req, res) => {
                 currency: "SEK"
             }
         }),
-        success_url: req.protocol + '://' + req.get('Host') + '/thankyou', //SKRIV HEROKU URL!!!
+        success_url: req.protocol + '://' + req.get('Host') + '/thankyou',
         cancel_url: "http://localhost:8000/felsida"
 
     }).then((session) => {
